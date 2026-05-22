a9b4dea36a129eaa7fb372fbe743c52374f8a95d
Lana Topchii <topchii.svitlana@gmail.com>
2026-05-23 01:05:20 +0300

    fix: regenerate migration, fix quote style, add trailing newlines

--- cinema/migrations/0001_initial.py
+++ cinema/migrations/0001_initial.py
@@ -14,7 +14,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Movie',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    )
+                ),
                 ('title', models.CharField(max_length=255)),
                 ('description', models.CharField(max_length=255)),
                 ('duration', models.IntegerField()),
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,4 +6,4 @@ from cinema.models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ("id", "title", "description", "duration")
+        fields = ('id', 'title', 'description', 'duration')
